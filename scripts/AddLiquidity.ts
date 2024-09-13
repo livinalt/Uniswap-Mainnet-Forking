6b6332f858a95f54ba7bfd924a933460e24f0429
Livinalt <livinalt@gmail.com>
2024-09-13 22:17:46 +0100

    fixes::: fixed Add Liquidity script

--- scripts/AddLiquidity.ts
+++ scripts/AddLiquidity.ts
@@ -3,36 +3,36 @@ const helpers = require("@nomicfoundation/hardhat-network-helpers");
 
 async function main() {
     const ROUTER_ADDRESS = "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D";
-    const USDC = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48";
-    const DAI = "0x6B175474E89094C44Da98b954EedeAC495271d0F";
+    const LIDO = "0x5A98FcBEA516Cf06857215779Fd812CA3beF1B32";
+    const PEPE = "0x6982508145454Ce325dDbE47a25d4ec3d2311933";
 
-    const TOKEN_HOLDER = "0xf584F8728B874a6a5c7A8d4d387C9aae9172D621";
+    const TOKEN_HOLDER = "0xF977814e90dA44bFA03b6295A0616a897441aceC";
 
     // Impersonating the account
     await helpers.impersonateAccount(TOKEN_HOLDER);
     const impersonatedSigner = await ethers.getSigner(TOKEN_HOLDER);
 
-    const USDC_Contract = await ethers.getContractAt("IERC20", USDC, impersonatedSigner);
-    const DAI_Contract = await ethers.getContractAt("IERC20", DAI);
+    const LIDO_Contract = await ethers.getContractAt("IERC20", LIDO, impersonatedSigner);
+    const PEPE_Contract = await ethers.getContractAt("IERC20", PEPE, impersonatedSigner);
     
     const ROUTER = await ethers.getContractAt("IUniswapV2Router", ROUTER_ADDRESS, impersonatedSigner);
 
     // Check balances before adding liquidity
-    const USDCBalBefore = await USDC_Contract.balanceOf(impersonatedSigner.address);
-    const daiBalBefore = await DAI_Contract.balanceOf(impersonatedSigner.address);
+    const LIDOBalBefore = await LIDO_Contract.balanceOf(impersonatedSigner.address);
+    const PEPEBalBefore = await PEPE_Contract.balanceOf(impersonatedSigner.address);
 
-    console.log("USDC balance before adding liquidity:", Number(USDCBalBefore));
-    console.log("DAI balance before adding liquidity:", Number(daiBalBefore));
+    console.log("LIDO balance before adding liquidity:", Number(LIDOBalBefore));
+    console.log("PEPE balance before adding liquidity:", Number(PEPEBalBefore));
 
     // Approving the Uniswap Router to spend tokens on behalf of the impersonated account
-    const amountADesired = ethers.parseUnits("4", 6); // USDC is 6 decimals
-    const amountBDesired = ethers.parseUnits("2", 18); // DAI is 18 decimals
+    const amountADesired = ethers.parseUnits("4", 18); // LIDO is 6 decimals
+    const amountBDesired = ethers.parseUnits("2", 18); // PEPE is 18 decimals
 
-    await USDC_Contract.approve(ROUTER_ADDRESS, amountADesired);
-    await DAI_Contract.approve(ROUTER_ADDRESS, amountBDesired);
+    await LIDO_Contract.approve(ROUTER_ADDRESS, amountADesired);
+    await PEPE_Contract.approve(ROUTER_ADDRESS, amountBDesired);
 
     // Define slippage tolerance (e.g., 1% slippage)
-    const amountAMin = ethers.parseUnits("2", 6); // 1% less than desired
+    const amountAMin = ethers.parseUnits("1", 18); // 1% less than desired
     const amountBMin = ethers.parseUnits("1", 18); // 1% less than desired
 
     // Address to receive LP tokens
@@ -43,23 +43,23 @@ async function main() {
 
     // Add liquidity to the pool
     await ROUTER.addLiquidity(
-        USDC,
-        DAI,
+        LIDO, // token A
+        PEPE, // token B
         amountADesired, 
-        amountBDesired, // Desired amount of DAI
-        amountAMin,     // Minimum acceptable amount of USDC (with slippage tolerance)
-        amountBMin,     // Minimum acceptable amount of DAI (with slippage tolerance)
+        amountBDesired, // Desired amount of PEPE
+        amountAMin,     // Minimum acceptable amount of LIDO (with slippage tolerance)
+        amountBMin,     // Minimum acceptable amount of PEPE (with slippage tolerance)
         to,             // Address that will receive the LP tokens
         newDeadline     // Transaction deadline
     );
 
     // Check balances after adding liquidity
-    const USDCBalAfter = await USDC_Contract.balanceOf(impersonatedSigner.address);
-    const daiBalAfter = await DAI_Contract.balanceOf(impersonatedSigner.address);
+    const LIDOBalAfter = await LIDO_Contract.balanceOf(impersonatedSigner.address);
+    const PEPEBalAfter = await PEPE_Contract.balanceOf(impersonatedSigner.address);
 
     console.log("=========================================================");
-    console.log("USDC balance after adding liquidity:", Number(USDCBalAfter));
-    console.log("DAI balance after adding liquidity:", Number(daiBalAfter));
+    console.log("LIDO balance after adding liquidity:", Number(LIDOBalAfter));
+    console.log("PEPE balance after adding liquidity:", Number(PEPEBalAfter));
 
     console.log("Liquidity added successfully.");
 }
